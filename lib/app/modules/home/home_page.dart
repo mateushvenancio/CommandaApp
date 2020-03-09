ebe00a7f97d6e541cc53191a00a6e55e5ed40a3a
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2020-03-09 13:08:03 -0300

    initial commit

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -3,24 +3,40 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
-  final String title;
-  const HomePage({Key key, this.title = "Home"}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
-  //use 'controller' variable to access controller
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      body: Container(
+        color: Colors.red,
+        child: Column(
+          children: [
+            AppBar(
+              title: Text(
+                'Commanda',
+                style: TextStyle(
+                  fontFamily: 'Lobster',
+                ),
+              ),
+              centerTitle: true,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+            ),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  'Saiba mais',
+                  style: TextStyle(fontSize: 8),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/consts/const_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color primaryColor = Color(0xff);
+  static Color secondaryColor = Color(0xff);
+}
